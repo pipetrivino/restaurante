c777ee3f861cd26d4de72d77e231c0e769d3f416
pipetrivino <pipetrivino@gmail.com>
2025-07-15 16:19:15 -0500

    gff

--- api/api.php
+++ api/api.php
@@ -4,8 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use MongoDB\Client;
 
 // URI de conexión (puedes mover la contraseña a una variable de entorno si lo vas a hacer más seguro)
-$env = parse_ini_file(__DIR__ . '/config.env');
-$uri = $env['MONGODB_URI']
+$uri = 'mongodb+srv://pipe:pipe@clusterrestaurante.o6ivt52.mongodb.net/?retryWrites=true&w=majority&appName=Clusterrestaurante';
 
 $client = new Client($uri);
 
